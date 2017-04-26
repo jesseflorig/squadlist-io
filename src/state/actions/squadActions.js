7397019d356e154abde80b7525e4ebc633c942b3
Ramsay Lanier <rlanier@ALT8AG8WL.local>
2017-04-26 11:08:39 -0400

    updates to squad state

--- src/state/actions/squadActions.js
+++ src/state/actions/squadActions.js
@@ -1,14 +1,28 @@
 export const ACTIVATE_BUILDER = 'ACTIVATE_BUILDER'
 export const SET_FACTION = 'SET_FACTION'
+export const ADD_SHIP_TO_SQUAD = 'ADD_SHIP_TO_SQUAD'
+export const SET_ACTIVE_SHIP = 'SET_ACTIVE_SHIP'
 
-export function activateBuilder() {
+export function activateBuilder () {
   return {
     type: ACTIVATE_BUILDER
   }
 }
 
-export function setFaction(faction) {
+export function setFaction (faction) {
   return {
     type: SET_FACTION, faction
+  }
+}
+
+export function addShipToSquad (ship) {
+  return {
+    type: ADD_SHIP_TO_SQUAD, ship
+  }
+}
+
+export function setActiveShip (ship) {
+  return {
+    type: SET_ACTIVE_SHIP, ship
   }
 }
--- src/state/reducers/squadReducer.js
+++ src/state/reducers/squadReducer.js
@@ -1,11 +1,17 @@
-import { SET_FACTION, ACTIVATE_BUILDER } from '../actions/squadActions'
+import {
+  SET_FACTION,
+  ACTIVATE_BUILDER,
+  ADD_SHIP_TO_SQUAD } from '../actions/squadActions'
 
 const initialState = {
   isActive: false,
+  ships: [],
+  activeShip: null,
   faction: ''
 }
 
 export default function squad(state = initialState, action) {
+  const ships = state.ships
   switch (action.type) {
   case ACTIVATE_BUILDER:
     return Object.assign({}, state, {
@@ -14,6 +20,12 @@ export default function squad(state = initialState, action) {
   case SET_FACTION:
     return Object.assign({}, state, {
       faction: action.faction
+    })
+  case ADD_SHIP_TO_SQUAD:
+    ships.push(action.ship)
+    return Object.assign({}, state, {
+      ships: ships,
+      activeShip: action.ship.id
     })
   default:
     return state
